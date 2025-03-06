579eab815bbb649dde0303989f37369b20d0f9cc
VivienSoulas <poufpaf@Viv-XPS-15.>
2025-03-06 22:48:10 +0100

    start on new split

--- split.c
+++ split.c
@@ -10,10 +10,12 @@ int	ft_is_token(char c)
 		return (3);
 	if (c == ' ' || c == '\t')
 		return (-1);
+	if (c == 39 || c == 34)
+		return (10);
 	return (0);
 }
 
-char	**split_input(char *input)
+int word_count(char *input)
 {
 	int	in_word;
 	int	words;
@@ -24,6 +26,31 @@ char	**split_input(char *input)
 	i = 0;
 	while (input[i])
 	{
-
+		if (ft_is_token(input[i]) != 0 && in_word == 0)
+		{
+			words++;
+			in_word == 1;
+			if (ft_is_token(input[i + 1]) == 2 || ft_is_token(input[i + 1]) == 3)
+				i++;
+			else if (ft_is_token(input[i]) == 10)
+			{
+				while (ft_is_token(input[i]) == 10)
+					i++;
+			}
+			else if (ft_is_token(input[i]) == -1)
+			{
+				while (ft_is_token(input[i]) == -1)
+					i++;
+			}
+		}
+		else if (ft_is_token(input[i]) == 0 && in_word == 1)
+			in_word = 0;
+		else if (in_word == 0)
+		{
+			words++;
+			in_word = 1;
+		}
+		i++;
 	}
+	return (words);
 }
