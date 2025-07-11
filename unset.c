1b9766676938646bb83d0fe6816b29fa1de6865c
VivienSoulas <poufpaf@Viv-XPS-15.localdomain>
2025-07-11 20:40:54 +0200

    unset with multiple args

--- unset.c
+++ unset.c
@@ -12,32 +12,41 @@
 
 #include "parsing.h"
 
+void	ft_name_found(t_envp *prev, t_envp **list)
+{
+	prev = *list;
+	*list = (*list)->next;
+	free(prev->name);
+	free(prev->value);
+	return (free(prev));
+}
+
 void	unset(t_command *command, t_envp **list)
 {
 	t_envp	*current;
 	t_envp	*prev;
+	int		i;
 
-	if (!command->args[1])
+	i = 1;
+	if (!command->args[i])
 		return ;
-	if (!ft_strcmp((*list)->name, command->args[1]))
+	while (command->args[i])
 	{
-		prev = *list;
-		*list = (*list)->next;
-		free(prev->name);
-		free(prev->value);
-		return (free(prev));
-	}
-	current = *list;
-	prev = current;
-	while (current && ft_strcmp(current->name, command->args[1]))
-	{
+		if (!ft_strcmp((*list)->name, command->args[i]))
+			return(ft_name_found(prev, list));
+		current = *list;
 		prev = current;
-		current = current->next;
-	}
-	if (current)
-	{
-		prev->next = current->next;
-		ft_free_env(current);
+		while (current && ft_strcmp(current->name, command->args[i]))
+		{
+			prev = current;
+			current = current->next;
+		}
+		if (current)
+		{
+			prev->next = current->next;
+			ft_free_env(current);
+		}
+		i++;
 	}
 }
 
